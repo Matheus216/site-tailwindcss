1df05ab2ea172de25fbc461fc08e6d291761590e
Matheus216 <mrt.matheus.dev@gmail.com>
2023-06-17 16:30:12 -0300

    create project e add packages for use

---
+++ site-tailwindcss/src/app/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+      <div className="h-screen bg-cover bg-center bg-[url('/background.webp')]">
+        <h2 className="md:text-lg lg:text-lg sm:text-sm font-mono text-slate-400 p-10">
+          Texas dev web creative
+        </h2>
+        <h1 className="text-3xl font-mono text-center text-slate-300">
+          Welcome
+        </h1>
+        <div className="text-2xl font-mono text-center text-slate-300 m-10">
+          become your best version
+        </div>
+      </div>
+  )
+}
