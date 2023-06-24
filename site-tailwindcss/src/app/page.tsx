52d7c43536b778cd8617ab8829c9ea12e875a6b5
Matheus216 <mrt.matheus.dev@gmail.com>
2023-06-24 15:44:35 -0300

    Add class collapse

--- site-tailwindcss/src/app/page.tsx
+++ site-tailwindcss/src/app/page.tsx
@@ -3,13 +3,13 @@ import Navbar from "@/components/navbar/navbar";
 export default function Home() {
   return (
       <div className="h-screen bg-cover bg-center bg-[url('/background.webp')]">
-        <div className='relative ps-40 pe-40'>
+        <div className='relative ps-20 pe-20'>
           <Navbar />
         </div>
        <h1 className="text-3xl font-mono text-center text-slate-300 mt-20">
           Welcome
         </h1>
-        <div className="text-2xl font-mono text-center text-slate-300 m-10 bg-[#1a3c59a3] rounded-md">
+        <div className="text-2xl font-mono text-center text-slate-300 m-10 bg-[#19535da3] rounded-md">
           become your best version
         </div>
       </div>
--- site-tailwindcss/src/components/navbar/navbar.tsx
+++ site-tailwindcss/src/components/navbar/navbar.tsx
@@ -14,15 +14,16 @@ export default function Navbar() {
     return (
       <div className="w-full h-40 max-w-full items-center justify-between flex">
         <div className="md:text-lg lg:text-lg sm:text-sm font-mono text-slate-400 w-[20%]">
-          Texas dev web creative
+          <div className="md:hidden lg:hidden block text-2xl" >TDWC</div>
+          <div className="hidden md:block lg:block">Texas dev web creative</div>
         </div>
-        <ul className='md:flex lg:flex md:w-[30%] lg:w-[30%] justify-between items-center gap-10 w-0 hidden'>
+        <ul className='md:flex lg:flex md:w-[30%] lg:w-[30%] justify-between items-center gap-10 w-0 hidden sm:flex-col md:flex-row' >
           <ItemNav description='hero' href='#' icon={faHouse} />
           <ItemNav description='about' href='#' icon ={faBook}/>
           <ItemNav description='Services' href='#' icon={faBuilding} />
           <ItemNav description='Contact' href='#' icon={faPhone} />
         </ul>
-        <div className='w-[20%] text-slate-300'>
+        <div className='w-[20%] text-slate-300 text-center'>
           <div className='cursor-pointer hidden md:block lg:block'>Get started</div>
           <i className=' md:hidden lg:hidden'><a href='#'><FontAwesomeIcon className='h-6' icon={faBars} /></a></i>
         </div>
